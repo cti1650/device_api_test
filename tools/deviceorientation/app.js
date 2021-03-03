7d1e815d0476d92db962bd9b0692f12b1a0ad229
cti1650 <cti1650@gmail.com>
2021-03-03 19:55:38 +0900

    test

--- tools/deviceorientation/app.js
+++ tools/deviceorientation/app.js
@@ -1,41 +1,48 @@
-window.addEventListener("devicemotion", function (event) {
-  var x = event.accelerationIncludingGravity.x;
-  var y = event.accelerationIncludingGravity.y;
-  var z = event.accelerationIncludingGravity.z;
+const requestDeviceMotionPermission = async () => {
+  // ジャイロセンサーが使えるかどうか
+  if (window.DeviceOrientationEvent) {
+    // ios13以上
+    if (
+      DeviceOrientationEvent.requestPermission &&
+      typeof DeviceOrientationEvent.requestPermission === "function"
+    ) {
+      const permission = await DeviceMotionEvent.requestPermission();
+      if (permission === "granted") {
+        window.addEventListener("deviceorientation", deviceorientation);
+      } else {
+        window.alert("許可されていません");
+      }
+    } else {
+      // ios13未満
+      window.addEventListener("deviceorientation", deviceorientation);
+    }
+  } else {
+    window.alert("対応していません");
+  }
+};
 
-  var result1 = document.getElementById("result_acc");
-  result1.innerHTML =
-    "重力加速度<br />" +
-    "X：" +
-    x.toFixed(2) +
-    "(m/s^2)<br />" +
-    "Y：" +
-    y.toFixed(2) +
-    "(m/s^2)<br />" +
-    "Z：" +
-    z.toFixed(2) +
-    "(m/s^2)<br />";
-});
+let alpha = 0,
+  beta = 0,
+  gamma = 0;
 
-window.addEventListener(
-  "deviceorientation",
-  function (event) {
-    var alpha = event.alpha;
-    var beta = event.beta;
-    var gamma = event.gamma;
+const deviceorientation = (dat) => {
+  // iphoneとandroidで向きが逆なので-1を掛けて任意に修正
+  alpha = dat.alpha * -1;
+  beta = dat.beta * -1;
+  gamma = dat.gamma * -1;
+};
 
-    var result2 = document.getElementById("result_gyro");
-    result2.innerHTML =
-      "ジャイロセンサー<br />" +
-      "alpha：" +
-      alpha.toFixed(2) +
-      "°<br />" +
-      "beta ：" +
-      beta.toFixed(2) +
-      "°<br />" +
-      "gamma：" +
-      gamma.toFixed(2) +
-      "°<br />";
-  },
-  false
-);
+const handlePress = async () => {
+  try {
+    await requestDeviceMotionPermission();
+  } catch (e) {
+    console.error(e);
+    window.alert("対応していません");
+  }
+
+  const timer = window.setInterval(() => {
+    document.getElementById(
+      "txt"
+    ).innerHTML = `alpha: ${alpha}<br>beta: ${beta}<br>gamma: ${gamma}`;
+  });
+};
