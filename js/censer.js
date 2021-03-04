4943dbf971f0cab3f0819cc1433b32395879241f
cti1650 <cti1650@gmail.com>
2021-03-04 20:55:09 +0900

    test

--- js/censer.js
+++ js/censer.js
@@ -40,58 +40,49 @@ class DeviceSensor {
     func(rep);
     return false;
   }
-  setDeviceOrientationEvent() {
-    const requestDeviceMotionPermission = async () => {
-      // ジャイロセンサーが使えるかどうか
-      if (window.DeviceOrientationEvent) {
-        // ios13以上
-        if (
-          DeviceOrientationEvent.requestPermission &&
-          typeof DeviceOrientationEvent.requestPermission === "function"
-        ) {
-          const permission = await DeviceMotionEvent.requestPermission();
-          if (permission === "granted") {
-            window.addEventListener("deviceorientation", deviceorientation);
-          } else {
-            window.alert("許可されていません");
-          }
+  async setDeviceOrientationEvent(func) {
+    let rep = {};
+    // ジャイロセンサーが使えるかどうか
+    if (window.DeviceOrientationEvent) {
+      // ios13以上
+      if (
+        DeviceOrientationEvent.requestPermission &&
+        typeof DeviceOrientationEvent.requestPermission === "function"
+      ) {
+        const permission = await DeviceMotionEvent.requestPermission();
+        if (permission === "granted") {
+          window.addEventListener("deviceorientation", function (dat) {
+            // iphoneとandroidで向きが逆なので-1を掛けて任意に修正
+            rep = {
+              alpha: Math.round(dat.alpha * -1 * 1000) / 1000,
+              beta: Math.round(dat.beta * -1 * 1000) / 1000,
+              gamma: Math.round(dat.gamma * -1 * 1000) / 1000,
+            };
+            func(rep);
+          });
+          return true;
         } else {
-          // ios13未満
-          window.addEventListener("deviceorientation", deviceorientation);
+          // window.alert("許可されていません");
+          rep = { error: "許可されていません" };
         }
       } else {
-        window.alert("対応していません");
+        // ios13未満
+        window.addEventListener("deviceorientation", function (dat) {
+          // iphoneとandroidで向きが逆なので-1を掛けて任意に修正
+          rep = {
+            alpha: Math.round(dat.alpha * -1 * 1000) / 1000,
+            beta: Math.round(dat.beta * -1 * 1000) / 1000,
+            gamma: Math.round(dat.gamma * -1 * 1000) / 1000,
+          };
+          func(rep);
+        });
+        return true;
       }
-      return false;
-    };
-
-    let alpha = 0,
-      beta = 0,
-      gamma = 0;
-
-    const deviceorientation = (dat) => {
-      // iphoneとandroidで向きが逆なので-1を掛けて任意に修正
-      alpha = Math.round(dat.alpha * -1 * 100) / 100;
-      beta = Math.round(dat.beta * -1 * 100) / 100;
-      gamma = Math.round(dat.gamma * -1 * 100) / 100;
-      if (Math.round(beta) === 0) {
-        window.navigator.vibrate(200);
-      }
-    };
-
-    const handlePress = async () => {
-      try {
-        await requestDeviceMotionPermission();
-      } catch (e) {
-        console.error(e);
-        window.alert("対応していません");
-      }
-
-      const timer = window.setInterval(() => {
-        document.getElementById(
-          "txt"
-        ).innerHTML = `alpha: ${alpha}<br>beta: ${beta}<br>gamma: ${gamma}`;
-      }, 200);
-    };
+    } else {
+      // window.alert("対応していません");
+      rep = { error: "対応していません" };
+    }
+    func(rep);
+    return false;
   }
 }
--- tools/multi/app.js
+++ tools/multi/app.js
@@ -2,6 +2,7 @@ const ds = new DeviceSensor();
 
 function multiGetData() {
   const status = document.querySelector("#status");
+  const status2 = document.querySelector("#status2");
   const mapLink = document.querySelector("#map-link");
 
   if (
@@ -11,6 +12,23 @@ function multiGetData() {
           "x:" + data["x"] + "<br>y:" + data["y"] + "<br>z:" + data["z"];
       } else {
         status.innerHTML = data["error"];
+      }
+    })
+  ) {
+    alert("done");
+  }
+  if (
+    ds.setDeviceOrientationEvent(function (data) {
+      if (data["error"] === "none") {
+        status2.innerHTML =
+          "alpha:" +
+          data["alpha"] +
+          "<br>beta:" +
+          data["beta"] +
+          "<br>gamma:" +
+          data["gamma"];
+      } else {
+        status2.innerHTML = data["error"];
       }
     })
   ) {
